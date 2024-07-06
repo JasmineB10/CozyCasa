2af9adc37bd5fec331d5eabec0d72d98c85d0fc9
Jasmine Bajwa <146476804+JasmineB10@users.noreply.github.com>
2024-07-06 13:52:54 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,7 @@ module.exports.signUp = async(req,res)=> {
         if(err){
             return next(err);
         }
-        req.flash("success", "Welcome to Wanderlust!");
+        req.flash("success", "Welcome to CozyCasa!");
         res.redirect("/listings");
     })
     
